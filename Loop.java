a1ed16f75f55309ef5fee2775055234819395a86
Shreya Kumari <96626023+Shreya-Kumari21@users.noreply.github.com>
2023-04-22 16:24:51 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,13 @@
+public class Loop {
+    public static void main(String[] args) {
+        int n=20;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<10;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }   
+    }
+}
